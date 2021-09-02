0289b55072c93081f6d5a754da3ba8e6da176775
Kristín Þórðardóttir <kristint@brimborg.is>
2021-09-02 18:11:07 +0000

    max_int

---
+++ max_int.py
@@ -0,0 +1,3 @@
+# Design an algorithm that finds the maximum positive integer input by a user.  The user repeatedly inputs numbers until a negative value is entered.
+
+#Ask the user to input positive integer
